3930f0ca1e1e5a998056fc147d5e188d86532c7e
Michael Adam Groberman <michael.groberman@me.com>
2021-11-08 17:24:21 -0500

    created server.js file
    defined the port for our app to be served on, initiated the express app, defined the /api we are listening for

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
+// Require Express and Path
 const express = require("express");
 const path = require("path");
 
+// define express app and port to use
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
-// need to create
 const api = require("./routes/index.js");
 
 // Middleware for parsing JSON and urlencoded data
-app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extensions: true }));
-
+app.use("/api", api);
 app.use(express.static("public"));
 
+// GET method when user goes to the domain
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/")));
 
+// GET method for Notes, takes the user to the notes page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
-
+// Listen for api requests on defined port and print to the console
 app.listen(PORT, () => {
-  console.log("App listening at http://localhost:${PORT}");
+  console.log("App running at http://localhost:${PORT}");
 });
