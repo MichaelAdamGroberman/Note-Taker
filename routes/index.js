a24666b283b8365cd7ca1dd2740e4da09b09f3dc
Michael Adam Groberman <michael.groberman@me.com>
2021-11-08 17:20:02 -0500

    updated the index.js express script
    added comments, required notes express route module, defined '/notes' to use notesRoute

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
+// require Express package
 const express = require("express");
 
+// Import the notes module
+const notesRouter = require("./notes");
+// initiate express application
 const app = express();
 
+app.use("/notes", notesRouter);
+
+// export app as module to use elsewhere in the application
 module.exports = app;
